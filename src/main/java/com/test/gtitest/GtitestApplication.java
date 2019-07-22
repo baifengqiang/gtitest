54563c9035acf6cafcdaef2240f44779c359eb3a
柏凤强 <1020474227@qq.com>
2019-07-22 21:10:43 +0800

    Initial commit

---
+++ src/main/java/com/test/gtitest/GtitestApplication.java
@@ -0,0 +1,13 @@
+package com.test.gtitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtitestApplication.class, args);
+	}
+
+}
